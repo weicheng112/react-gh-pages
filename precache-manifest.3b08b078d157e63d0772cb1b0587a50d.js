0a8e40a1fedb7554a84bbeb48978d43dd7439c2d
weicheng112 <cjiyjpgji@gmail.com>
2024-02-05 16:11:40 -0800

    Deploy React app to GitHub Pages

---
+++ precache-manifest.3b08b078d157e63d0772cb1b0587a50d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba91e82554267df1faf36d26dd3b6217",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "ee888b5b91ffbf6a85f4",
+    "url": "/react-gh-pages/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "6525bf8fc4385bf3c7a2",
+    "url": "/react-gh-pages/static/js/2.bcc28277.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/react-gh-pages/static/js/2.bcc28277.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef90e41bcf13bdafb7b",
+    "url": "/react-gh-pages/static/js/3.82afb7a3.chunk.js"
+  },
+  {
+    "revision": "ee888b5b91ffbf6a85f4",
+    "url": "/react-gh-pages/static/js/main.ace13ff9.chunk.js"
+  },
+  {
+    "revision": "3581f5128d35fc2c9566",
+    "url": "/react-gh-pages/static/js/runtime-main.978a30f8.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-gh-pages/static/media/logo.06e73328.svg"
+  }
+]);
